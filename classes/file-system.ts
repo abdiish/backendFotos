8dbf4a73f4e1608bf8cf982f422665b41b2e463f
Alan Cortes <ing.ti.upmh@gmail.com>
2021-11-10 15:58:15 -0600

    Generar nombre unico archivo imagen

--- classes/file-system.ts
+++ classes/file-system.ts
@@ -1,15 +1,29 @@
 import { FileUpload } from '../interfaces/file-upload';
 import path from 'path';
 import fs from 'fs';
+import uniqid from 'uniqid';
 
 export default class FileSystem {
 
     constructor() {}
 
     guardarImagenTemporal(file: FileUpload, userId: string){
-
+        // Crear carpetas
         const path = this.crearCarpetaUsuario(userId);
+        // Nombre archivo
+        const nombreArchivo = this.generarNombreUnico(file.name);
+        console.log(nombreArchivo);
 
+    }
+
+    private generarNombreUnico(nombreOriginal: string) {
+
+        //Extraer extnesión del archivo
+        const nombreArr = nombreOriginal.split('.');
+        const extension = nombreArr[nombreArr.length - 1];
+        const idUnico = uniqid();
+
+        return `${idUnico}.${extension}`;
     }
 
     private crearCarpetaUsuario(userId: string) {
